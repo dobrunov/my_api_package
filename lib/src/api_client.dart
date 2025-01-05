39f8d306df4e3910d15ff94a481db01e6ee67710
Evgeniy D <e.dobrunov@gmail.com>
2025-01-05 10:51:48 +0200

    changing _processResponse

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -23,50 +23,20 @@ class ApiClient {
     return _processResponse(response);
   }
 
-  Future<Map<String, dynamic>> post(
-    String endpoint, {
-    Map<String, String>? headers,
-    Map<String, dynamic>? body,
-  }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.post(
-      uri,
-      headers: {...defaultHeaders, ...?headers},
-      body: jsonEncode(body),
-    );
-    return _processResponse(response);
-  }
-
-  Future<Map<String, dynamic>> put(
-    String endpoint, {
-    Map<String, String>? headers,
-    Map<String, dynamic>? body,
-  }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.put(
-      uri,
-      headers: {...defaultHeaders, ...?headers},
-      body: jsonEncode(body),
-    );
-    return _processResponse(response);
-  }
-
-  Future<void> delete(
-    String endpoint, {
-    Map<String, String>? headers,
-  }) async {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.delete(uri, headers: {...defaultHeaders, ...?headers});
-    _processResponse(response);
-  }
-
-  Map<String, dynamic> _processResponse(http.Response response) {
+  Future<dynamic> _processResponse(http.Response response) async {
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      return jsonDecode(response.body);
+      try {
+        return jsonDecode(response.body);
+      } catch (e) {
+        throw ApiException(
+          statusCode: response.statusCode,
+          message: 'Failed to decode JSON: $e',
+        );
+      }
     } else {
       throw ApiException(
         statusCode: response.statusCode,
-        message: response.body,
+        message: 'Error ${response.statusCode}: ${response.body}',
       );
     }
   }
